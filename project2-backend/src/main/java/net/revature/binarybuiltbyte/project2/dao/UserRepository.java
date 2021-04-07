9419b276bd89f65d8cf91369fece1ce93c51a9d9
Ronald (Jeff) Lopez <32562609+lopezjronald@users.noreply.github.com>
2021-04-07 14:22:54 -0700

    updated application.properties to connect with heroku database

--- project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/dao/UserRepository.java
+++ project2-backend/src/main/java/net/revature/binarybuiltbyte/project2/dao/UserRepository.java
@@ -2,6 +2,5 @@ package net.revature.binarybuiltbyte.project2.dao;
 
 import net.revature.binarybuiltbyte.project2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
